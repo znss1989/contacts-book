3ba4b144da8d6c6dd55b40950b817e3037cc850a
Wu, Lei <znss1989@gmail.com>
2019-08-03 10:12:35 +0800

    have dummy routes for register, login & contacts

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   });
 });
 
+// routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
