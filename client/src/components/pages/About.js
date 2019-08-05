c2a14319d1ed47b4a488d69468e4408793c73ff1
Wu, Lei <znss1989@gmail.com>
2019-08-05 16:13:58 +0800

    implement about

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,7 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      About
+      <h1>About </h1>
+      <p className="bg-light p" style={{ textAlign: "right" }}>
+        <strong>Version: </strong> 1.0.8
+      </p>
+      <p className="my-3">
+        <strong>Feather Contacts &trade;</strong> is a web application serving as a user-based contact book.
+      </p>
+
     </div>
   )
 };
