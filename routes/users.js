3e423cfae4f483c1b288597cbb2f62d7173b70e8
Wu, Lei <znss1989@gmail.com>
2019-08-05 10:05:56 +0800

    sign & return jwt token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -45,7 +47,17 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      return res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(payload, config.get('jwtSecret'), { 
+        expiresIn: 10800 
+      }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      })
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
