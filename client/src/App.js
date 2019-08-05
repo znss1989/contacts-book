fddbb387080355c327a5b41e0b5a2455e68b5997
Wu, Lei <znss1989@gmail.com>
2019-08-05 15:57:13 +0800

    have navbar & style up

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello, Contacts Book
+      <Navbar />
     </div>
   );
 }
