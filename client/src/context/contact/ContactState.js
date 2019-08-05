14218a3a446b0961fc1fbf2e6dbcd802ee724dd1
Wu, Lei <znss1989@gmail.com>
2019-08-05 19:28:54 +0800

    hot fix

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,18 +1,18 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 
 import ContactContext from './contactContext';
 import contactReucer from './contactReducer';
 
-import { 
-  ADD_CONTACT,
-  DELETE_CONTACT,
-  SET_CURRENT,
-  CLEAR_CURRENT,
-  UPDATE_CONTACT,
-  FILTER_CONTACTS,
-  CLEAR_FILTER
-} from '../types';
+// import { 
+//   ADD_CONTACT,
+//   DELETE_CONTACT,
+//   SET_CURRENT,
+//   CLEAR_CURRENT,
+//   UPDATE_CONTACT,
+//   FILTER_CONTACTS,
+//   CLEAR_FILTER
+// } from '../types';
 
 const ContactState = props => {
   const initialState = {
@@ -56,4 +56,4 @@ const ContactState = props => {
   );
 };
 
-export default ContactContext;
+export default ContactState;
