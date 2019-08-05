fe3ccefd01a1117f420a1867e3991041d4258a8e
Wu, Lei <znss1989@gmail.com>
2019-08-05 16:02:56 +0800

    basic routing set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Fragment} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={ Home } />
+            <Route exact path='/about' component={ About } />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
