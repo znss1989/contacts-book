15b2f08d2e2bc00d0801398678989fb704bf50c4
Wu, Lei <znss1989@gmail.com>
2019-08-08 15:18:06 +0800

    bring in alerts

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import ContactState from './context/contact/ContactState';
 import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Home from './components/pages/Home';
@@ -21,6 +22,7 @@ function App() {
             <Fragment>
               <Navbar />
               <div className="container">
+                <Alerts />
                 <Switch>
                   <Route exact path='/register' component={ Register } />
                   <Route exact path='/login' component={ Login } />
