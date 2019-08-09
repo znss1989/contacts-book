fde1ab4548f1262ea298880bf97cd80986c2ccb1
Wu, Lei <znss1989@gmail.com>
2019-08-09 11:31:57 +0800

    update contact in database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -70,17 +70,29 @@ const ContactState = props => {
     }
   };
 
-  const updateContact= contact => {
-    dispatch({
-      type: UPDATE_CONTACT,
-      payload: contact
-    });
+  const updateContact = async contact => {
+    const config = { 
+      headers: { 'Content-Type': 'application/json' }
+    };
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
   }
 
   const deleteContact = async _id => {
     try {
       await axios.delete(`/api/contacts/${_id}`);
-      
+
       dispatch({
         type: DELETE_CONTACT,
         payload: _id
